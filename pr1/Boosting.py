5b0ca12db528979f00b72cd1764f0191d6e7d9dd
yma2022 <youlong.ma@gmail.com>
2023-09-23 14:03:38 -0400

    pr1 boost update range

--- pr1/Boosting.py
+++ pr1/Boosting.py
@@ -18,13 +18,13 @@ class BoostClassifier(object):
 
     def train(self, X_train, y_train):
         # set up a weak learner
-        base_clf = DecisionTreeClassifier(max_depth=3, min_samples_leaf=1, min_samples_split=5, ccp_alpha=0.0005, random_state=42)
+        base_clf = DecisionTreeClassifier(max_depth=3, ccp_alpha=0.0005, random_state=42)
         base_clf.fit(X_train, y_train)
         print("At depth:", base_clf.tree_.max_depth)
         print("Base Decision Tree Accuracy on the Train set: ", base_clf.score(X_train, y_train))
         clf_boost = AdaBoostClassifier(base_clf, random_state=42)
         params = {'n_estimators': np.arange(1, 200, 20),
-                  'learning_rate': np.logspace(-3, 0, 5)}
+                  'learning_rate': np.logspace(-3, 1, 6)}
 
         gcv = GridSearchCV(estimator=clf_boost,param_grid=params, cv=4)
         gcv.fit(X_train,y_train)
@@ -35,7 +35,7 @@ class BoostClassifier(object):
         if self.verbose:
             util.plot_learning_curve(self.dataset, estimator=best_dt, X_train=X_train, y_train=y_train, param_range=np.linspace(0.2,1,5, endpoint=True), title="Learning Curve for Boosting Decision Tree")
             util.plot_validation_curve(self.dataset, best_dt, X_train, y_train, title="Validation Curve for Boosting Decision Tree over n_estimators", xlabel='n_estimators', p_name="n_estimators", p_range=np.arange(1, 200, 20), cv=4)
-            util.plot_validation_curve(self.dataset, best_dt, X_train, y_train, title="Validation Curve for Boosting Decision Tree over learning_rate", xlabel='learning_rate', p_name="learning_rate", p_range=np.logspace(-3, 0, 5), cv=4, log=True)
+            util.plot_validation_curve(self.dataset, best_dt, X_train, y_train, title="Validation Curve for Boosting Decision Tree over learning_rate", xlabel='learning_rate', p_name="learning_rate", p_range=np.logspace(-3, 1, 6), cv=4, log=True)
 
         start = time.time()
         best_dt.fit(X_train, y_train)
