cc675d3a887806ae025f5692cf5c8eb91aa6fbd2
yma2022 <youlong.ma@gmail.com>
2023-09-23 17:55:08 -0400

    pr1 figure update

--- pr1/SVM.py
+++ pr1/SVM.py
@@ -20,41 +20,51 @@ class SupportVectorMachine(object):
         decision tree initialization
         """
         self.verbose = verbose
-        self.clf = None
+        self.clf = []
         self.dataset = dataset
 
     def train(self, X_train, y_train):
         """
         build and train decision tree
         """
-        clf_svm = SVC(random_state=42)
-        param_grid = {'C': np.logspace(-5, 5, 10),
-                      'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-                      'gamma': np.logspace(-5, 5, 10)}
-        clf_svm.fit(X_train,y_train)
-        print("SVM Accuracy on the Train set: ", clf_svm.score(X_train, y_train))
+        train_scores = []
+        train_time = []
+        best_scores = []
+        for k in ['poly', 'rbf', 'sigmoid']:
+            clf_svm = SVC(kernel=k, random_state=42)
+            param_grid = {'C': np.logspace(-10, 5, 10),
+                        'gamma': np.logspace(-10, 5, 10)}
+            clf_svm.fit(X_train,y_train)
+            train_scores.append(clf_svm.score(X_train, y_train))            
 
-        grid_search = GridSearchCV(estimator=clf_svm, param_grid=param_grid, cv=4)
+            grid_search = GridSearchCV(estimator=clf_svm, param_grid=param_grid, cv=4)
 
-        grid_search.fit(X_train, y_train)
-        best_svm = grid_search.best_estimator_
-        print("Best parameters:", grid_search.best_params_)
-        
+            grid_search.fit(X_train, y_train)
+            best_svm = grid_search.best_estimator_
+            print("Best parameters:", grid_search.best_params_)
+            
 
 
-        if self.verbose:
-            util.plot_learning_curve(self.dataset, best_svm, X_train, y_train, np.linspace(0.2,1,5, endpoint=True), title="Learning Curve for SVM")
-            util.plot_validation_curve(self.dataset, best_svm, X_train, y_train, title="Validation Curve for SVM over C", xlabel='C', p_name="C", p_range=np.logspace(-5, 10, 10), ylim=(0, 1.01), cv=4, log = True)
-            util.plot_validation_curve(self.dataset, best_svm, X_train, y_train, title="Validation Curve for SVM over gamma", xlabel='gamma', p_name="gamma", p_range=np.logspace(-5, 10, 10), ylim=(0, 1.01), cv=4, log = True)
+            if self.verbose:
+                util.svm_plot_learning_curve(self.dataset, best_svm, X_train, y_train, np.linspace(0.2,1,5, endpoint=True), title="Learning Curve for SVM", kernel=k)
+                util.svm_plot_validation_curve(self.dataset, best_svm, X_train, y_train, title="Validation Curve for SVM over C", xlabel='C', kernel=k, p_name="C", p_range=np.logspace(-10, 10, 10), ylim=(0, 1.01), cv=4, log = True)
+                util.svm_plot_validation_curve(self.dataset, best_svm, X_train, y_train, title="Validation Curve for SVM over gamma", xlabel='gamma', kernel=k, p_name="gamma", p_range=np.logspace(-10, 10, 10), ylim=(0, 1.01), cv=4, log = True)
 
-        start = time.time()
-        best_svm.fit(X_train, y_train)
-        end = time.time()
-        print("SVM Training time:", end - start)
-        print("Best SVM Accuracy on the Train set: ", best_svm.score(X_train, y_train))
-        self.clf = best_svm
+            start = time.time()
+            best_svm.fit(X_train, y_train)
+            end = time.time()
+            train_time.append(end - start)
+            best_scores.append(best_svm.score(X_train, y_train))           
+            self.clf.append(best_svm)
+
+        print("SVM Accuracy on the Train setwith linear, poly, rbf and sigmoid: ", train_scores)
+        print("SVM Training time with linear, poly, rbf and sigmoid:", train_time)
+        print("Best SVM Accuracy on the Train set with linear, poly, rbf and sigmoid: ", best_scores)
 
     def query(self, X_test, y_test):
-        y_pred = self.clf.predict(X_test)
-        test_score = accuracy_score(y_test, y_pred)
-        print("SVM Accuracy on the Test set: ", test_score)
+        res = []
+        for clf in self.clf:
+            y_pred = clf.predict(X_test)
+            test_score = accuracy_score(y_test, y_pred)
+            res.append(test_score)
+        print("SVM Accuracy on the Test set with linear, poly, rbf and sigmoid: ", res)
