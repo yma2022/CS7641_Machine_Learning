41ffc74b5110ada9c1e0bb6669625441296d2e24
yma2022 <youlong.ma@gmail.com>
2023-10-14 11:47:04 -0400

    pr2 update

---
+++ pr2/four_peaks.py
@@ -0,0 +1,92 @@
+import numpy as np
+import mlrose_hiive as mlrose
+import time
+import util
+import random
+
+SEEDs = [42, 24, 7, 100, 200, 300, 400, 500, 600, 700]
+
+fitness = mlrose.FourPeaks(t_pct=0.15)
+problem = mlrose.DiscreteOpt(length=40, fitness_fn=fitness, maximize=True, max_val=2)
+
+# Select and run a randomized optimization algorithm.
+
+
+
+
+iterations = [5, 50, 100, 200, 500, 750, 1000, 1250]
+data_time = []
+data_fitness = []
+
+restarts = util.rhc_tuning(problem)
+
+process_time_rhc = [0] * len(iterations)
+best_fitness_rhc = [0] * len(iterations)
+for k in range(10):
+    for i, iter in enumerate(iterations):
+        start = time.time()
+        best_state, best_fitness, fitness_curve1 = mlrose.random_hill_climb(problem, max_attempts = 100, max_iters = iter, curve = True,random_state=SEEDs[k], restarts=restarts)
+        stop = time.time()
+        process_time_rhc[i] += stop - start
+        best_fitness_rhc[i] += best_fitness
+        if k == 9:
+            process_time_rhc[i] /= 10
+            best_fitness_rhc[i] /= 10
+data_time.append(process_time_rhc)
+data_fitness.append(best_fitness_rhc)
+
+decay_param = util.sa_tuning(problem)
+# Define decay schedule
+schedule = mlrose.ExpDecay(init_temp=decay_param[0], exp_const=decay_param[1], min_temp=decay_param[2])
+process_time_sa = [0] * len(iterations)
+best_fitness_sa = [0] * len(iterations)
+for k in range(10):
+    for i, iter in enumerate(iterations):
+        start = time.time()
+        best_state, best_fitness, fitness_curve2 = mlrose.simulated_annealing(problem, schedule = schedule,max_attempts = 100, max_iters = iter, curve = True,random_state = SEEDs[k])
+        stop = time.time()
+        process_time_sa[i] += stop - start
+        best_fitness_sa[i] += best_fitness
+        if k == 9:
+            process_time_sa[i] /= 10
+            best_fitness_sa[i] /= 10
+data_time.append(process_time_sa)
+data_fitness.append(best_fitness_sa)
+
+pop_ga, mutation_p = util.ga_tuning(problem)
+process_time_ga = [0] * len(iterations)
+best_fitness_ga = [0] * len(iterations)
+for k in range(10):
+    for i, iter in enumerate(iterations):
+        start = time.time()
+        best_state, best_fitness, fitness_curve3 = mlrose.genetic_alg(problem, pop_size=pop_ga, mutation_prob=mutation_p, max_attempts = 100, max_iters = iter, curve = True, random_state = SEEDs[k])
+        stop = time.time()
+        process_time_ga[i] += stop - start
+        best_fitness_ga[i] += best_fitness
+        if k == 9:
+            process_time_ga[i] /= 10
+            best_fitness_ga[i] /= 10
+data_time.append(process_time_ga)
+data_fitness.append(best_fitness_ga)
+
+
+pop_m, keep_p = util.mimic_tuning(problem)
+process_time_mimic = [0] * len(iterations)
+best_fitness_mimic = [0] * len(iterations)
+for k in range(10):
+    for i, iter in enumerate(iterations):
+        start = time.time()
+        best_state, best_fitness, fitness_curve4 = mlrose.mimic(problem, pop_size=pop_m, keep_pct=keep_p, max_attempts = 100, max_iters = iter, curve = True, random_state = SEEDs[k])
+        stop = time.time()
+        process_time_mimic[i] += stop - start
+        best_fitness_mimic[i] += best_fitness
+        if k == 9:
+            process_time_mimic[i] /= 10
+            best_fitness_mimic[i] /= 10
+data_time.append(process_time_mimic)
+data_fitness.append(best_fitness_mimic)
+
+util.plot_problem(data_fitness, iterations, ylabel="Best Fitness", title="Four Peaks Optimal by Iterations: Average of 10", show=False)
+util.plot_problem(data_time, iterations, ylabel="Process Time", title="Four Peaks Process Time: Average of 10", show=False)
+
+
---
+++ pr2/util.py
@@ -0,0 +1,108 @@
+# utility class for the project
+from matplotlib import pyplot as plt
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import validation_curve
+import numpy as np
+import mlrose_hiive as mlrose
+import itertools
+import time
+
+
+def plot_curve(data, param_range, ylabel="", title="", show=False):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("# of Iterations")
+    plt.ylabel(ylabel)
+    labels = ["Back Prop", "RHC", "SA", "GA"]
+    colors = ['r', 'g', 'b', 'y']
+    for i in range(len(data)):
+        plt.plot(param_range, data[i],'o-', color=colors[i], label=labels[i])
+    plt.grid()
+    plt.legend(loc="best")
+    if show:
+        plt.show()
+    else:
+        outfile = "img/" + title + ".png"
+        plt.savefig(outfile)
+        plt.close()
+
+
+def plot_problem(data, param_range, ylabel="", title="", show=False):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("# of Iterations")
+    plt.ylabel(ylabel)
+    labels = ["RHC", "SA", "GA", "MIMIC"]
+    colors = ['r', 'g', 'b', 'y']
+    for i in range(len(data)):
+        plt.plot(param_range, data[i],'o-', color=colors[i], label=labels[i])
+    plt.grid()
+    plt.legend(loc="best")
+    if show:
+        plt.show()
+    else:
+        outfile = "img/" + title + ".png"
+        plt.savefig(outfile)
+        plt.close()
+
+def rhc_tuning(problem):
+    restarts = None
+    best_fitness = None
+    for i in [0, 25, 50, 75, 100]:
+        _, rhc_best_fitness, _ = mlrose.random_hill_climb(problem, max_attempts = 100, max_iters = 1000, curve = True,random_state=42, restarts=i)
+        if not restarts:
+            restarts = i
+            best_fitness = rhc_best_fitness
+        elif rhc_best_fitness > best_fitness:
+            restarts = i
+            best_fitness = rhc_best_fitness
+    return restarts
+
+def sa_tuning(problem):
+    #init_temp, exp_const, min_temp
+    best_param = None
+    best_fitness = None
+    sa_hyperparams = [[1, 2, 4, 8, 16, 32, 64],[0.1, 0.2, 0.4, 0.8],[0.001, 0.01, 0.1, 1]]
+
+    for i in itertools.product(*sa_hyperparams):
+        decay = mlrose.ExpDecay(init_temp = i[0], exp_const=i[1], min_temp=i[2])
+        _, sa_best_fitness, _ = mlrose.simulated_annealing(problem, schedule = decay,max_attempts = 100, max_iters = 1000, curve = True,random_state=42)
+        if not best_param:
+            best_param = i
+            best_fitness = sa_best_fitness
+        elif sa_best_fitness > best_fitness:
+            best_param = i
+            best_fitness = sa_best_fitness
+    return best_param
+
+def ga_tuning(problem):
+    #pop_size, mutation_prob
+    best_param = None
+    best_fitness = None
+    ga_hyperparams = [[50, 100, 200, 400, 800],[0.1, 0.2, 0.4, 0.8]]
+
+    for i in itertools.product(*ga_hyperparams):
+        _, ga_best_fitness, _ = mlrose.genetic_alg(problem, pop_size=i[0], mutation_prob=i[1], max_attempts=100, max_iters=1000, curve=True, random_state=42)
+        if not best_param:
+            best_param = i
+            best_fitness = ga_best_fitness
+        elif ga_best_fitness > best_fitness:
+            best_param = i
+            best_fitness = ga_best_fitness
+    return best_param
+
+def mimic_tuning(problem):
+    #pop_size, keep_pct
+    best_param = None
+    best_fitness = None
+    mimic_hyperparams = [[50, 100, 200, 400, 800],[0.1, 0.2, 0.4, 0.8]]
+
+    for i in itertools.product(*mimic_hyperparams):
+        _, mimic_best_fitness, _ = mlrose.mimic(problem, pop_size=i[0], keep_pct=i[1], max_attempts=100, max_iters=1000, curve=True, random_state=42)
+        if not best_param:
+            best_param = i
+            best_fitness = mimic_best_fitness
+        elif mimic_best_fitness > best_fitness:
+            best_param = i
+            best_fitness = mimic_best_fitness
+    return best_param
